dbb5747f8f0abcf5c3658e90f8078b91d17280ae
AnaVukojicic <ana.vukojicic2011@gmail.com>
2022-12-30 15:06:17 +0100

    Fixed bug in DesktopIndex page

--- src/pages/desktopIndex/DesktopIndex.jsx
+++ src/pages/desktopIndex/DesktopIndex.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CardSection from "../cardSection/CardSection";
 import FooterSection from "../footerSection/FooterSection";
-import IntroductionSection from "../introductionSection/IntroductionSection";
 import classes from './DesktopIndex.module.scss';
 
 
